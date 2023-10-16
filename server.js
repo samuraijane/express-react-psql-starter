ae111e29ae8e6b131bb60e5ea5edc45c9c2afe14
Matthew Day <matt@matthewday.net>
2023-10-16 07:29:52 -0400

    handle CORS and add a POST endpoint to write new users to the db

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const bcrypt = require('bcrypt');
-const express = require('express');
-const path = require('path');
-const pgp = require('pg-promise')();
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const express = require("express");
+const path = require("path");
+const pgp = require("pg-promise")();
 
 const server = express();
 
@@ -11,7 +11,8 @@ const {
   DB_NAME,
   DB_PASSWORD,
   DB_PORT,
-  DB_USER,  
+  DB_USER,
+  NODE_ENV,
   PORT
 } = process.env;
 
@@ -26,10 +27,19 @@ const cn = {
 
 const db = pgp(cn);
 
+if (NODE_ENV === "development") {
+  server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+    next();
+  });
+}
+
 server.use(express.json());
 server.use(express.static(path.resolve(`${__dirname}/react-ui/build`)));
 
-server.get('/heartbeat', (req, res) => {
+server.get("/heartbeat", (req, res) => {
   res.json({
     "is": "working"
   })
@@ -38,20 +48,54 @@ server.get('/heartbeat', (req, res) => {
 // TODO
 //   delete this endpoint before deploying as it exists for demo purposes only;
 //   not deleting this endpoint puts the database at risk.
-server.get('/users', async (req, res) => {
+server.get("/users", async (req, res) => {
   try {
-    const users = await db.query('SELECT * FROM users');
+    const users = await db.query("SELECT * FROM users");
     res.json({ users });
   } catch (error) {
     res.json({ error });
   }
 });
 
+server.post("/signup", async (req, res) => {
+  const {
+    email,
+    firstname,
+    lastname,
+    password
+  } = req.body;
+
+  try {
+    const newuser = await db.query(`
+      INSERT INTO users (last_name, first_name, email, pw_hash, created_at, updated_at) VALUES
+      ('${lastname}', '${firstname}', '${email}', '${password}', CURRENT_TIMESTAMP(3), CURRENT_TIMESTAMP(3))
+      ON CONFLICT (email) DO NOTHING
+      RETURNING email;
+    `);
+    
+    if (!newuser.length) {
+      res.json({
+        isSuccess: false,
+        message: "This email is already registered."
+      });
+    } else if (newuser[0].email === email) {
+      res.json({
+        isSuccess: true,
+        message: `User with email ${email} has been successfully registered.`
+      });
+    } else {
+      throw new Error("Something went wrong.");
+    }
+  } catch (error) {
+    res.json({ error });
+  }
+});
+
 // delegate client-side routing to the client
-server.get('*', (req, res) => {
+server.get("*", (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/react-ui/build/index.html)`));
 });
 
 server.listen(process.env.PORT || PORT, () => {
-  console.log(`The server is listening at port ${PORT}`);
-})
+  console.log(`The server is running in a ${NODE_ENV} environment and listening at port ${PORT}`);
+});
